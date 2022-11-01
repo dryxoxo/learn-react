996ee9f95515731bd863aaa7d815299d61006b8f
ibrohimhuzaimi <ibrohimhuzaimi@gmail.com>
2022-11-01 22:29:29 +0700

    validate proptypes

---
+++ react-proptypes/src/components/StudentInfo.jsx
@@ -0,0 +1,15 @@
+import PropTypyes from "prop-types";
+
+export default function StudentInfo({ name, age }) {
+  return (
+    <>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+    </>
+  );
+}
+
+StudentInfo.propTypes = {
+  name: PropTypyes.string,
+  age: PropTypyes.number,
+};
